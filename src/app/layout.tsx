b341e44f50475a66cdf5ab2cefcb7e6243ec63dc
Sm-Ashraful <smashraful.dev@gmail.com>
2024-05-05 15:07:05 +0600

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/views/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} overflow-x-hidden overflow-y-auto pt-10`}
+      >
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,16 +5,23 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/views/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      dark: "#04060B",
+      black: "#000000",
+      lightDark: "#090F1A",
+      darkerWhite: "#FBFCFE",
+      darkGold: "#B08B40",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        "space-grotesk": ["Space Grotesk", "sans-serif"],
       },
     },
   },
+
   plugins: [],
 };
 export default config;
